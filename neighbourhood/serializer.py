376b173cecf7d1686e169c06078b171143859ef5
wangechimk <wangechik.michelle@gmail.com>
2021-06-06 21:41:48 +0300

    add the serializer from model fields

--- neighbourhood/serializer.py
+++ neighbourhood/serializer.py
@@ -1,20 +1,38 @@
 from django.db import models
 from django.db.models import fields
 from rest_framework import serializers
-from .models import Neighbourhood,User,Business
+from rest_framework.fields import ReadOnlyField
+from .models import Neighbourhood,User,Business,Post,Profile
+from django import forms
 
-
-class NeighbourhoodSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Neighbourhood
-    fields=['name','location']
-
-class UserSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = User
-    exclude = ['neighbourhood']  
 
 class BusinessSerializers(serializers.ModelSerializer):
   class Meta:
     model = Business
-    fields=['name','email']      
+    fields='__all__'   
+
+class ProfileSerializer(serializers.ModelSerializer):
+  business = BusinessSerializers(many=True, read_only=True)
+  # User= serializers.CharField(read_only=True)
+
+  class Meta:
+    model = Profile
+    fields="__all__"
+
+class NeighbourhoodSerializer(serializers.ModelSerializer):
+  profile = ProfileSerializer(many=True, read_only=True)
+  business = BusinessSerializers(many=True, read_only=True)
+  class Meta:
+    model = Neighbourhood
+    fields='__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+  email = forms.EmailField(max_length=300, help_text='Required. Inform a valid email address.')
+  class Meta:
+    model = User
+    fields = ['username','email','password']
+ 
+class PostSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = Post
+    fields = "__all__" 
